69c77545f67b6d4765c9b50fb59f5780f92a78f5
DrugJangSu <ljonghyun6088@gmail.com>
2026-08-10 16:03:57 +0100

    Update

--- java-masterclass/HelloWorld.java
+++ java-masterclass/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld {
 ///// Section 3 : First Steps in Java Programming --------------
 //// Your First Java Program : Displaying "Hello World" and Basic Error Handling
 /* // on the terminal
+% jshell
 |  Welcome to JShell -- Version 17.0.20
 |  For an introduction type: /help intro
 jshell> System.out.print("Hello World");
@@ -20,3 +21,11 @@ jshell> System.out.print("Hello World");
 jshell> System.out.print("Hello Tim");
 Hello Tim
  */
+// ...> is when jshell notices that you haven't fully typed the code, and is waiting for you to finish the code.
+/* 
+jshell> System.out.print("Hello Tim); // in this case it shows an error.
+|  Error:
+|  unclosed string literal
+|  System.out.print("Hello Tim);
+|                   ^
+*/
