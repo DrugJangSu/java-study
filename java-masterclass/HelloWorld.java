cbf463b042fab16324e20ef6f06d5afc1a8cb189
DrugJangSu <ljonghyun6088@gmail.com>
2026-08-21 14:55:48 +0100

    Update

--- java-masterclass/HelloWorld.java
+++ java-masterclass/HelloWorld.java
@@ -1491,8 +1491,8 @@ result -= 5.5; → 자동으로 (int) 형변환이 일어남
     - and team development and etc.
 
 /// What is Intellij?
-// IntelliJ IDEA is one of several IDEs available for Java.
-// It's also written in Java developed by JetBrains and simply known as IntelliJ.
+IntelliJ IDEA is one of several IDEs available for Java.
+It's also written in Java developed by JetBrains and simply known as IntelliJ.
 JetBrains offer a free and open source community edition.
 
 
