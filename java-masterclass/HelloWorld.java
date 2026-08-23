d62a4776cdfd4ec2822546c025d2f6cfcc7ecd61
DrugJangSu <ljonghyun6088@gmail.com>
2026-08-23 22:17:32 +0100

    Update

--- java-masterclass/HelloWorld.java
+++ java-masterclass/HelloWorld.java
@@ -1526,6 +1526,13 @@ And IDE is a program that provides comprehensive facilities to programmers for s
 
 //// Creating Your First IntelliJ Project - "Hello World" Java Code Implementation ---------------------------------------------------------------
 
+/// Naming Items in Java
+// Camel case is the practice of capitalising the first letters of words in a name for readability, removing spaces or characters such as underscores between the words. Lower camel case only capitalises the letter of the second and subsequent words. Upper camel case also known as Pascal case, capitalises the first letter of the first word as well.
+(Identifier) => (Usage) => (Recommended) => (Example)
+Project Name => IntelliJ Field => Upper Camel Case => FirstJavaProject
+Class Name => Java element => Upper Camel Case => NewClass
+Method Name => Java element => Lower Camel Case => getData
+Variable Name => Java element => Lower Camel Case => firstVariable
 
 
 */
