3c241934e8a35837c2053a542708c127e56a2724
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-07 13:39:22 +0900

    Update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -2019,7 +2019,37 @@ boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
 
 
 //// Java Operator Precedence : Mastering Expression Evaulation and Challenge ---------------------------------------------------------------
-/// You can go to th
+/// Challenge
+
+// Step 1 : create a double variable with a value of 20.00
+
+// Step 2 : create a second variable of type double with a value 80.00
+
+// Step 3 : add both numbers together then multiply by 100.00
+
+// Step 4 : use the remainder operator to figure out what the remainder from the result of the operation in step 3, and 40.00 will be.
+
+// Step 5 : create a boolean variable that assigns the value true, if the remainder in step 4 is 0.00 or faluse if it's not zero.
+
+// Step 6 : output the boolean variable just to see what the result is.
+
+// Step 7 : write an if-then statement that displays a message, 'got some remainder' if the boolean in step 5 is not true.
+
+// Tim's answer ---------------------------
+double myFirstValue = 20.00d;
+double mySecondValue = 80.00d;
+double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+System.out.println("MyValuesTotal = " + myValuesTotal);
+double theRemainder = myValuesTotal % 40.00d;
+System.out.println("theRemainder = " + theRemainder);
+boolean isNoRemainder = (theRemainder == 0) ? true : false;
+System.out.println("isNoRemainder = " + isNoRemainder);
+if (!isNoRemainder) {
+    System.out.println("Got some remainder");
+    }
+
+
+
 
 
 */
