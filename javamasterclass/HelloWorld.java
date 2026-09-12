2c5715a32fa86da8afed0df80be95c9abff105b1
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-12 17:01:08 +0900

    Update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -2296,6 +2296,30 @@ ex)
 */
 //// Practical If-Then-Else Challenge For Java Flow Control ---------------------------
 /*
+    public class MainChallenge {
+
+        public static void main(String[] args) {
+
+            boolean gameOver = true;
+            int score = 800;
+            int levelCompleted = 5;
+            int bonus = 100;
+
+            int finalScore = score;
+            if (gameOver) { // gameOver == true
+                finalScore += (levelCompleted * bonus); // finalScore = finalScore + (levelCompleted * bonus)
+                System.out.println("Your final score was " + finalScore);
+            }
+        }
+    }
+
+
+/// If then else Challenge
+    Insert a code segment on the code above:
+        - Set the existing score variable to 10,000
+        - Set the existing levelCompleted variable to 8.
+        - Set the existing bonus variable to 200.
+        - Use the same if condition. Meaning if gameOver is true, then you want to perform the same calculation and print out the value of the finalScore variable.
 
 
 */
