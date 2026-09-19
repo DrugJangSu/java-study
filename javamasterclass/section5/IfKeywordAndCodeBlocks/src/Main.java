dbdd291e960079d265e1d34b86f611a97e20f9e2
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-19 23:46:39 +0900

    Update

---
+++ javamasterclass/section5/IfKeywordAndCodeBlocks/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5000;
+    }
+}
--- javamasterclass/section5/StatementsWhiteSpaceAndIndenting/src/Main.java
+++ javamasterclass/section5/StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -11,5 +11,9 @@ public class Main {
 
         int anotherVariable = 50; myVariable--;
         System.out.println("myVariable = " + myVariable);
+
+        if (myVariable == 0) {
+            System.out.println("It's now zero.");
+        }
     }
 }
