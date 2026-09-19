2891fc847f79d66fb4d6bbb74c200f1553d035f9
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-19 22:43:08 +0900

    Update

--- javamasterclass/section5/KeywordsAndExpressions/src/Main.java
+++ javamasterclass/section5/KeywordsAndExpressions/src/Main.java
@@ -1,17 +1,8 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
 
     public static void main(String[] args) {
-        double kilometers = (100 * 1.609344);
 
-        int highScore = 50;
-
-        if(highScore > 25) {
-            highScore = 1000 + highScore; // add bonus points
-        }
-        int health = 100;
-
-        if ((health < 25) && (highScore > 1000)) {
-            highScore = highScore - 1000;
-        }
     }
 }
