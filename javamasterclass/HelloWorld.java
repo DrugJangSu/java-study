c72841a47f4fc3392d3f044f82e1a59a40537a1f
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-20 01:17:18 +0900

    update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -2543,6 +2543,43 @@ Some programming languages will call a method that returns a value, a function, 
 The term procedure is somewhat less common when applied to Java methods, but you may still hear a method with a void return type called procedure.
 
 
+/// Declaring the Method
+So there are quite a few declarations that need to occur when creating a method.
+This consists of;
+    - Declaring Modifiers
+        These are keywords in Java with special meanings, like public and static as examples.
+    - Declaring the return type.
+        void is a Java keyword meaning no data is returned from a method.
+        Alternatively, the return type can be any primitive data type or class.
+        If a return type is defined, the code block must use at least one return statement, returning a value of the declared type or comparable type.
+    - Declaring the method name. Lower camel case is recommended for method names.
+    - Declaring the method parameters in parentheses. A method is not required to have parameters, so a set of empty parentheses would be declared in that case.
+    - Declaring the method block with opening and closing curly braces. This is also called the method body.
+
+/// Declaring the Parameters
+Parameters are declared as a list of comma-separated specifiers, each of which has a parameter type and a parameter name (or identifier).
+Parameter order is important when calling the method.
+The calling code must pass arguments to the method with the same or comparable type, and in the same order as the declaration.
+The calling code must pass the same number of arguments as the number of parameters declared.
+
+/// Declaring the Return Type
+When declaring a return type:
+void is a vaild return type and means no data is returned.
+Any other return type requires a return statement in the method code block.
+
+/// The Return Statement for methods that have a return type
+If a method declares a return type meaning it's not a void, then a return type is required at any exit point from the method block.
+Consider the method block shown here;
+    public static boolean isTooYoung(int age) {
+    if (age < 21) {
+        return true;
+    }
+}
+
+
+
+
+
 
 
  */
