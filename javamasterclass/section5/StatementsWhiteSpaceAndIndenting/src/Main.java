30c89a2072212d1445b95fe75b39879f50d9ae20
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-19 22:53:09 +0900

    Update

--- javamasterclass/section5/StatementsWhiteSpaceAndIndenting/src/Main.java
+++ javamasterclass/section5/StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -1,22 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
 
+    int myVariable = 50;
+    
 
-        int myVariable = 50;
-
-        myVariable++;
-        myVariable--;
-        System.out.println("This is a test");
-
-        System.out.println("This is" + " another" + " still more.");
-
-        int anotherVariable = 50;
-        myVariable--;
-        System.out.println("myVaraible = " + myVariable);
-
-        if (myVariable == 0) {
-            System.out.println("It's now zero");
-        }
     }
 }
