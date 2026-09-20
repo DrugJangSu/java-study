2d6292ee2c8917608d915a2cbc0ccf0954808dbd
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-20 18:16:37 +0900

    update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -2931,7 +2931,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getDurationString(3945));
-        System.out.println(getDurationString(,));
+        System.out.println(getDurationString(65, 45));
     }
     public static String getDurationString(int seconds) {
         // Two step approach to get hours
@@ -2962,15 +2962,24 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getDurationString(3945));
-        System.out.println(getDurationString(,));
+        System.out.println(getDurationString(65, 45));
     }
     public static String getDurationString(int seconds) {
-        // Two step approach to get hours
+        If (seconds <0) {
+            return "Invalid data for seconds("+ seconds + "), must be a positive integer value";
+        }
         int minutes = seconds / 60;
         return getDurationString(minutes, seconds);
     }
 
     public static String getDurationString(int minutes, int seconds) {
+
+        If (minutes < 0) {
+            return "Invalid data for minutes("+ minutes + "), must be a positive integer value";
+        }
+        if (seconds <0 || seconds > 59) {
+            return "Invalid data for seconds(" + seconds + "), must be between 0 and 59";
+        }
         int hours = minutes / 60;
 
         int remainingMinutes = minutes % 60;
@@ -2981,6 +2990,36 @@ public class Main {
         return "";
     }
 }
+
+------VS------
+<내 코드>
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getDurationString(200));
+        System.out.println(getDurationString(350, 25));
+    }
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            System.out.println("Invalid value detected. the value should be 0 or above.");
+            return "";
+        }   else {
+            return getDurationString(seconds/60, seconds%60);
+        }
+    }
+
+
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0 || seconds < 0 || seconds > 59) {
+            System.out.println("Invalid value detected. the value should be 0 or above, and seconds shouldn't exceed 59.");
+            return "";
+        } else {
+
+            return ((minutes/60)+ "h " + (minutes%60 + seconds/60) +"m " + (seconds%60) + "s");
+        }
+    }
+}
+
+
  */
 
 
