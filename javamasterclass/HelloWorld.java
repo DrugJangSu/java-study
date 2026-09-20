33a0987b4e53aacc021596027ee3075f33fcdafe
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-20 19:03:32 +0900

    update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -3035,6 +3035,34 @@ public class Main {
             // Code for value not equal to x or y
         }
 
+    public class Main {
+    public static void main(String[] args) {
+//        int value = 2;
+//        if (value == 1) {
+//            System.out.println("Value was 1");
+//        } else if (value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 4;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+        // More code here
+
+    }
+}
 
 
 
