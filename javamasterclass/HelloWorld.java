07802ffc8354a5b1ef4bc03305bb7b47875bc6ac
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-20 14:18:33 +0900

    update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -2681,7 +2681,7 @@ public class MethodChallenge {
     }
 
 
-    public static void calculateHighScorePosition(int score) {
+    public static int calculateHighScorePosition(int score) {
         if (score >= 1000) {
             System.out.println("1");
         } else if (score >= 500) {
@@ -2693,5 +2693,42 @@ public class MethodChallenge {
         }
     }
 }
+    <Tim's solution>
+public class MethodChallenge {
+    public static void main(String[] args) {
 
+
+    int highScorePosition = calculateHighScorePosition(1500);
+    displayHighScorePosition("Tim", highScorePosition);
+
+    highScorePosition = calculateHighScorePosition(1000);
+    displayHighScorePosition("Bob", highScorePosition);
+
+    highScorePosition = calculateHighScorePosition(500);
+    displayHighScorePosition("Percy", highScorePosition);
+
+    highScorePosition = calculateHighScorePosition(100);
+    displayHighScorePosition("Gilbert", highScorePosition);
+
+    highScorePosition = calculateHighScorePosition(25);
+    displayHighScorePosition("James", highScorePosition);
+
+    }
+    public static void displayHighScorePosition(String name, int highScorePosition) {
+        System.out.println(name + " managed to get into position " + highScorePosition + " on the high score list.");
+    }
+
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
  */
--- javamasterclass/section5/FirstMethodChallenge/src/MethodChallenge.java
+++ javamasterclass/section5/FirstMethodChallenge/src/MethodChallenge.java
@@ -1,35 +1,37 @@
 public class MethodChallenge {
     public static void main(String[] args) {
 
-    String name;
-    int position;
-    int score;
 
+    int highScorePosition = calculateHighScorePosition(1500);
+    displayHighScorePosition("Tim", highScorePosition);
 
-    displayHighScorePosition("Tim", 2);
-    calculateHighScorePosition(1500);
-    calculateHighScorePosition(1000);
-    calculateHighScorePosition(500);
-    calculateHighScorePosition(100);
-    calculateHighScorePosition(25);
+    highScorePosition = calculateHighScorePosition(1000);
+    displayHighScorePosition("Bob", highScorePosition);
 
+    highScorePosition = calculateHighScorePosition(500);
+    displayHighScorePosition("Percy", highScorePosition);
 
+    highScorePosition = calculateHighScorePosition(100);
+    displayHighScorePosition("Gilbert", highScorePosition);
+
+    highScorePosition = calculateHighScorePosition(25);
+    displayHighScorePosition("James", highScorePosition);
 
     }
-    public static void displayHighScorePosition(String name, int position) {
-        System.out.println(name + " managed to get into position " + position + " on the high score list.");
+    public static void displayHighScorePosition(String name, int highScorePosition) {
+        System.out.println(name + " managed to get into position " + highScorePosition + " on the high score list.");
     }
 
 
-    public static void calculateHighScorePosition(int score) {
+    public static int calculateHighScorePosition(int score) {
         if (score >= 1000) {
-            System.out.println("1");
+            return 1;
         } else if (score >= 500) {
-            System.out.println("2");
+            return 2;
         } else if (score >= 100) {
-            System.out.println("3");
+            return 3;
         } else {
-            System.out.println("4");
+            return 4;
         }
     }
 }
