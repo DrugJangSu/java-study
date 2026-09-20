1e34d16bb85b679dd8a660d84087417b896aac13
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-20 16:43:47 +0900

    update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -2732,7 +2732,121 @@ public class MethodChallenge {
     }
 }
  */
-//// Comparing And Reviewing Code in IntelliJ For Better Development ------------------------
+//// Delving Into Method Overloading in Java ------------------------
 /*
+/// Method Overloading
+Method overloading occurs when a class has multiple methods with the same name, but the methods are declared with different parameters.
+So you can execute multiple methods with the same name, but call it with different arguments.
+Java can resolve which method it needs to execute based on the arguments being passed when the method is invoked.
+
+/// More on Method Signatures
+A method signature consists of the name of the method, and the uniqueness of the declaration of its parameters.
+In other words, a signature is unique, not just by the method name but in combination with the number of parameters, their types, and the order in which they are declared.
+A method's return type is not part of the signature.
+A parameter name is also not part of the signature.
+
+/// Valid Overloaded Methods
+The type, order, and number of parameters in conjunction with the name, make a method signature unique.
+A unique method signature is the key for the Java compiler, to determine if a method is overloaded correctly.
+The name of the parameter is not part of the signature, and therefore it doesn't matter from Java's point of view, what we call our parameters.
+    ex)
+    public static void doSomething(int parameterA) {
+        // method body
+    }
+    public static void doSomething(float parameterA) {
+        // method body
+    }
+    public static void doSomething(int parameterA, float parameterB) {
+        // method body
+    }
+    public static void doSomething(int parameterA, int parawmeterB, float parameterC) {
+        // method body
+    }
+
+/// Invalid Overloaded Methods
+Parameter names are not important when determining if a method is overloaded.
+Nor are return types used when determining if a method is unique.
+    public static void doSomething(int parameterA) {
+        // method body
+    }
+    public static void doSomething(int parameterB) {
+        // method body
+    }
+    public static int doSomething(int parameterA) {
+        return 0;
+    }
+
+    ex)
+    public class Main {
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+    }
+    public static int calculateScore(String playerName, int score) {
+
+        System.out.println("Player " + playerName + " scored " + score + " points!");
+        return score * 1000;
+        }
+    }
+
+    ex)
+    public class Main {
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+
+        calculateScore(75);
+        calculateScore();
+    }
+    public static int calculateScore(String playerName, int score) {
+
+        System.out.println("Player " + playerName + " scored " + score + " points!");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+
+        System.out.println("Unnamed Player scored " + score + " points!");
+        return score * 1000;
+    }
+    public static int calculateScore() {
+
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
+}
+    ex)
+    public class Main {
+    public static void main(String[] args) {
+//        int newScore = calculateScore("Tim", 500);
+//        System.out.println("New score is " + newScore);
+//
+//        calculateScore(75);
+//        calculateScore();
+        System.out.println("New score is " + calculateScore("Tim", 500));
+        System.out.println("New score is " + calculateScore(10));
+    }
+    public static int calculateScore(String playerName, int score) {
+
+        System.out.println("Player " + playerName + " scored " + score + " points!");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+
+        return calculateScore("Anonymous", score);
+    }
+    public static int calculateScore() {
+
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
+}
+
+
+
+
+
+
 
  */
