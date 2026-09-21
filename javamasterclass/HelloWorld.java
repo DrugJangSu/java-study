d72692152682928dbdd13df6e1c2d9f75a49225f
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-21 22:03:09 +0900

    update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -3384,11 +3384,188 @@ These parts are all optional and consist of the following:
     - The expression section once it becomes false, will end the loop processing.
     - The increment section is executed after the expression is tested, and is generally the place where the loop variable is incremented.
 
+    public class Main {
+    public static void main(String[] args) {
+
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println(counter);
+        }
 
 
+        for (double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmount = calculateInterest(10000.0, rate);
+            System.out.println("10,000 at  " + rate + "% interest = " + interestAmount);
+        }
+
+    }
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+       }
+    }
+    <Mini Challenge>
+    <My Solution>
+    public class Main {
+    public static void main(String[] args) {
+
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println(counter);
+        }
+
+
+        for (double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmount = calculateInterest(10000.0, rate);
+            System.out.println("10,000 at  " + rate + "% interest = " + interestAmount);
+        }
+
+        for (double rate = 7.5; rate <= 10; rate = rate + 0.25) {
+            double interestAmount2 = calculateInterest(100.0, rate);
+            System.out.println("100 at  " + rate + "% interest = " + interestAmount2);
+        }
+
+
+    }
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+}
+
+
+/// The break statement
+A break statement transfers control out of an enclosing statement.
+
+    public class Main {
+    public static void main(String[] args) {
+
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println(counter);
+        }
+
+
+        for (double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmount = calculateInterest(10000.0, rate);
+            System.out.println("10,000 at  " + rate + "% interest = " + interestAmount);
+        }
+
+        for (double rate = 7.5; rate <= 10; rate = rate + 0.25) {
+            double interestAmount2 = calculateInterest(100.0, rate);
+            if (interestAmount2 > 8.5) {
+                break;
+            }
+            System.out.println("100 at  " + rate + "% interest = " + interestAmount2);
+        }
+
+
+    }
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+        }
+    }
 
 
  */
+//// Comprehensive for Loop Challenge To Strengthen Iteration Skills ----------------------------------------
+/*
+/// The for Statement Challenge Exercise
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int wholeNumber = 0; wholeNumber <= 1000; wholeNumber++) {
+            System.out.println(wholeNumber + " is " + (isPrime(wholeNumber) ? "" : "NOT ") + "a prime number.");
+        }
+
+//        System.out.println("0 is " + (isPrime(0) ? "" : "NOT ") + "a prime number");
+//        System.out.println("1 is " + (isPrime(1) ? "" : "NOT ") + "a prime number");
+//        System.out.println("2 is " + (isPrime(2) ? "" : "NOT ") + "a prime number");
+//        System.out.println("3 is " + (isPrime(3) ? "" : "NOT ") + "a prime number");
+//        System.out.println("4 is " + (isPrime(4) ? "" : "NOT ") + "a prime number");
+//        System.out.println("5 is " + (isPrime(5) ? "" : "NOT ") + "a prime number");
+//        System.out.println("8 is " + (isPrime(8) ? "" : "NOT ") + "a prime number");
+//        System.out.println("17 is " + (isPrime(17) ? "" : "NOT ") + "a prime number");
+//        System.out.println("31 is " + (isPrime(31) ? "" : "NOT ") + "a prime number");
+//        System.out.println("32 is " + (isPrime(32) ? "" : "NOT ") + "a prime number");
+
+    }
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+        for (int divisor = 2; divisor <= wholeNumber / 2; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+}
+
+    <Final Code>
+    public class Main {
+    public static void main(String[] args) {
+
+        int count = 0;
+        for (int i = 10; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println("number " + i + " is a prime number");
+                count++;
+                if (count == 3) {
+                    System.out.println("Found 3 - Exiting for loop");
+                    break;
+                }
+            }
+        }
+        }
+
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+        for (int divisor = 2; divisor <= wholeNumber / 2; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+
+        return true;
+        }
+    }
+
+    <shorter final code>
+    public class Main {
+    public static void main(String[] args) {
+
+        int count = 0;
+        for (int i = 10; count < 3 && i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println("number " + i + " is a prime number");
+                count++;
+
+            }
+        }
+
+        }
+
+
+
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+        for (int divisor = 2; divisor <= wholeNumber / 2; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+
+        return true;
+        }
+    }
+
+*/
+//// Sum 3 And 5 Challenge With IntelliJ Debugger Insights  ----------------------------------------
 
 
 
--- javamasterclass/section6/TheForLoop/src/Main.java
+++ javamasterclass/section6/TheForLoop/src/Main.java
@@ -6,7 +6,19 @@ public class Main {
         }
 
 
-        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        for (double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmount = calculateInterest(10000.0, rate);
+            System.out.println("10,000 at  " + rate + "% interest = " + interestAmount);
+        }
+
+        for (double rate = 7.5; rate <= 10; rate = rate + 0.25) {
+            double interestAmount2 = calculateInterest(100.0, rate);
+            if (interestAmount2 > 8.5) {
+                break;
+            }
+            System.out.println("100 at  " + rate + "% interest = " + interestAmount2);
+        }
+
 
     }
     public static double calculateInterest(double amount, double interestRate) {
