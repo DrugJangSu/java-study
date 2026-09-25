07f50c85bedf3f2d1f134e4489c8dcc650b9e9c1
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-25 13:45:22 +0900

    update

--- javamasterclass/HelloWorld.java
+++ javamasterclass/HelloWorld.java
@@ -3669,30 +3669,55 @@ the continue statement- in its simpliest form will stop executing the current it
 //// Practical while Loop Exercises And Challenges ----------------------------------------
 
 /*
+/// My Solution
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println(isEvenNumber(3));
+//        int number = 5;
+//        while (number <= 20) {
+//            if (isEvenNumber(number)) {
+//                System.out.println(number);
+//            }
+//            number++;
+//        }
+//
+//    }
+//
+//    public static boolean isEvenNumber(int number) {
+//        if (number % 2 == 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//}
+//
+
+/// Tim's Solution
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isEvenNumber(3));
-        int number = 5;
-        while (number <= 20) {
-            if (isEvenNumber(number)) {
-                System.out.println(number);
-            }
-            number++;
-        }
+        int number = 4;
+        int finishNumber = 20;
 
+        while (number <= finishNumber) {
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even number " + number);
+        }
     }
 
     public static boolean isEvenNumber(int number) {
-        if (number % 2 == 0) {
+        if ((number % 2) == 0) {
             return true;
         } else {
             return false;
         }
     }
+
+
 }
-
-
-
  */
 
 
--- javamasterclass/section6/WhileLoopChallenge/src/Main.java
+++ javamasterclass/section6/WhileLoopChallenge/src/Main.java
@@ -1,22 +1,49 @@
+/// My Solution
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println(isEvenNumber(3));
+//        int number = 5;
+//        while (number <= 20) {
+//            if (isEvenNumber(number)) {
+//                System.out.println(number);
+//            }
+//            number++;
+//        }
+//
+//    }
+//
+//    public static boolean isEvenNumber(int number) {
+//        if (number % 2 == 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//}
+//
+
+/// Tim's Solution
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isEvenNumber(3));
-        int number = 5;
-        while (number <= 20) {
-            if (isEvenNumber(number)) {
-                System.out.println(number);
-            }
-            number++;
-        }
+        int number = 4;
+        int finishNumber = 20;
 
+        while (number <= finishNumber) {
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even number " + number);
+        }
     }
 
     public static boolean isEvenNumber(int number) {
-        if (number % 2 == 0) {
+        if ((number % 2) == 0) {
             return true;
         } else {
             return false;
         }
     }
-}
 
+
+}
