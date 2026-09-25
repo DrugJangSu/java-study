45635939975b719c45c598f7aa3dea66499729dd
DrugJangSu <ljonghyun6088@gmail.com>
2026-09-25 11:02:37 +0900

    Update

--- javamasterclass/section6/Sum3And5Challenge/src/Main.java
+++ javamasterclass/section6/Sum3And5Challenge/src/Main.java
@@ -9,12 +9,20 @@
 public class Main {
 
     public static void main(String[] args) {
-    int count = 0;
-    int sum = 0;
-    for (int i = 1; i <= 1000; i++) {
-        
+
+    int countOfMatches = 0;
+    int sumOfMatches = 0;
+
+    for (int loopNumber = 1; loopNumber <= 1000; loopNumber++) {
+        if ((loopNumber % 3 == 0) && (loopNumber % 5 == 0)) {
+            countOfMatches++;
+            sumOfMatches += loopNumber;
+            System.out.println("Found a match = " + loopNumber);
+        }
+        if (countOfMatches == 5) {
+            break;
+        }
     }
-
-
+        System.out.println("Sum = " + sumOfMatches);
     }
 }
